7142937c37af2f519797eb882a789fa2bf271663
SYA0618 <polarisbabysea@gmail.com>
2025-04-20 15:58:39 +0800

    [Feature] Implement Add Command and Complete Database Structure
    - Implemented add [service] [password] command
    - Designed and completed the internal database logic
    - Finalized the data structure for password storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,63 @@
 package storage
+
+import (
+	"encoding/json"
+	"errors"
+	"passwordManger/internal/model"
+
+	"go.etcd.io/bbolt"
+)
+
+var db *bbolt.DB
+
+const dbFile = "passman.db"
+const bucketName = "passwords"
+
+func InitDB() error {
+	var err error
+	db, err = bbolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return db.Update(func(tx *bbolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+		return err
+	})
+}
+
+func SaveEntry(entry model.Entry) error {
+	return db.Update(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+		if b == nil {
+			return errors.New("bucket not found")
+		}
+		data, err := json.Marshal(entry)
+		if err != nil {
+			return err
+		}
+		return b.Put([]byte(entry.Service), data)
+	})
+}
+
+func GetEntry(service string) (model.Entry, error) {
+	var entry model.Entry
+	err := db.View(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+		if b == nil {
+			return errors.New("bucket not found")
+		}
+		data := b.Get([]byte(service))
+		if data == nil {
+			return errors.New("entry not found")
+		}
+		return json.Unmarshal(data, &entry)
+	})
+	return entry, err
+}
+
+func Close() {
+	if db != nil {
+		db.Close()
+	}
+}
